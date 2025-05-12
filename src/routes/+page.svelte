00d8ce015a82c260feed5721742009ed182444b3
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-05-12 15:32:02 -0700

    work on font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import GraphDavid from '$lib/GraphDavid.svelte';
-
+	// import GraphDavid from '$lib/GraphDavid.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<GraphDavid />
