333dab176b11b8934f5a746cf488cf8a01411858
bcan13 <canbrian59@gmail.com>
2025-05-12 15:30:21 -0700

    lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import * as d3 from 'd3';
+	import GraphBrian from '$lib/GraphBrian.svelte';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<GraphBrian></GraphBrian>
