fa5ca7e4e8a1544793acc6e1805ce8484c4c3e27
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-05-12 16:21:11 -0700

    work on css

---
+++ src/lib/Container.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		class?: string;
+		children?: Snippet<[]>;
+	}
+
+	let { class: classes, children }: Props = $props();
+</script>
+
+<div class={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${classes && classes}`}>
+	{@render children?.()}
+</div>
+
+<style scoped>
+	@reference '$lib/../app.css';
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Container from '$lib/Container.svelte';
 	import Header from '$lib/Header.svelte';
 	import '../app.css';
 	import '../scroll.css';
@@ -14,5 +15,24 @@
 
 <Header />
 <main>
-	{@render children()}
+	<Container>
+		<div class="flex flex-col gap-x-2.5">
+			<h1 class="title">Mouse Data; A Better Title Here</h1>
+			<h2>A cool insight of TBD</h2>
+			<h3 class="subtitle">Brian Can, David Chew, Derek Kuang, Aathi Muthu</h3>
+		</div>
+
+		<p>temp writeup here</p>
+
+		{@render children()}
+
+		<p>temp writeup here</p>
+	</Container>
 </main>
+
+<style scoped>
+	@reference "$lib/../app.css";
+	.title {
+		@apply font-serif text-4xl font-bold;
+	}
+</style>
