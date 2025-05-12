f0011c8a180fb9f4434e2e52fcb28d3e3c02d93d
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-05-12 16:34:02 -0700

    make  markdown area

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,23 +14,6 @@
 
 <main>
 	<Container>
-		<div class="flex flex-col gap-x-2.5">
-			<h1 class="title">Mouse Data; A Better Title Here</h1>
-			<h2>A cool insight of TBD</h2>
-			<h3 class="subtitle">Brian Can, David Chew, Derek Kuang, Aathi Muthu</h3>
-		</div>
-
-		<p>temp writeup here</p>
-
 		{@render children()}
-
-		<p>temp writeup here</p>
 	</Container>
 </main>
-
-<style scoped>
-	@reference "$lib/../app.css";
-	.title {
-		@apply font-serif text-4xl font-bold;
-	}
-</style>
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import * as d3 from 'd3';
-	import GraphBrian from '$lib/GraphBrian.svelte';
-</script>
-
-
-<GraphBrian></GraphBrian>
